1fd38a1b20dc79597830f50a041789831b4be15e
Matt Randolph <mttrndlph@gmail.com>
2020-12-27 19:05:54 -0500

    Finally got quiz to go to next question, working on getting high scores to show correctly

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,6 +2,7 @@
 var currentQuestionIndex = 0;
 var time = questions.length * 15;
 var timerId;
+var score = 0;
 
 // variables to reference DOM elements
 var questionsEl = document.getElementById("questions");
@@ -12,18 +13,19 @@ var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
-// sound effects
-var sfxRight = new Audio("assets/sfx/correct.wav");
-var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
 function startQuiz() {
   // hide start screen
-
+  document.getElementById("start-screen").style.display = "none";
   // un-hide questions section
-
+  questionsEl.style.display = "block";
   // start timer
-
+  timerId = setInterval(function () {
+    time--;
+    timerEl.textContent = time;
+  }, 1000);
   // show starting time
+  timerEl.textContent = time;
 
   getQuestion();
 }
@@ -33,22 +35,32 @@ function getQuestion() {
   var currentQuestion = questions[currentQuestionIndex];
 
   // update title with current question
-
+  document.getElementById("question-title").textContent = currentQuestion.title;
   // clear out any old question choices
 
-  // loop over choices
-  currentQuestion.choices.forEach(function(choice, i) {
+  choicesEl.innerHTML = ""
+
+  // loop over choices ALPER
+  currentQuestion.choices.forEach(function (choice, i) {
     // create new button for each choice
 
+    var newButton = document.createElement("Button");
+    choicesEl.appendChild(newButton);
+    newButton.textContent = choice;
+    
+
     // attach click event listener to each choice
-
+    newButton.setAttribute("onclick", "questionClick(event)")
+      
+    });
     // display on the page
-  });
-}
+    
+  };
 
-function questionClick() {
+
+function questionClick(event) {
   // check if user guessed wrong
-  if (this.value !== questions[currentQuestionIndex].answer) {
+  if (event.currentTarget.innerHTML !== questions[currentQuestionIndex].answer) {
     // penalize time
     time -= 15;
 
@@ -56,25 +68,22 @@ function questionClick() {
       time = 0;
     }
 
-    // display new time on page
-
-    // play "wrong" sound effect
-
     feedbackEl.textContent = "Wrong!";
   } else {
-    // play "right" sound effect
+  
 
     feedbackEl.textContent = "Correct!";
+    score++;
   }
 
   // flash right/wrong feedback on page for half a second
   feedbackEl.setAttribute("class", "feedback");
-  setTimeout(function() {
+  setTimeout(function () {
     feedbackEl.setAttribute("class", "feedback hide");
   }, 1000);
 
   // move to next question
-
+  currentQuestionIndex++;
   // check if we've run out of questions
   if (currentQuestionIndex === questions.length) {
     quizEnd();
@@ -85,13 +94,13 @@ function questionClick() {
 
 function quizEnd() {
   // stop timer
-
+  clearInterval(timerId);
   // show end screen
-
+  document.getElementById("end-screen").style.display= "block";
   // show final score
-
+  document.getElementById("final-score").innerHTML= score
   // hide questions section
-  questionsEl.setAttribute("class", "hide");
+  questionsEl.style.display = "none";
 }
 
 function clockTick() {
@@ -107,18 +116,20 @@ function clockTick() {
 
 function saveHighscore() {
   // get value of input box
-
+  var initials = initialsEl.nodeValue
   // make sure value wasn't empty
   if (initials !== "") {
     // get saved scores from localstorage, or if not any, set to empty array
-
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
     // format new score object for current user
     var newScore = {
-      score: time,
-      initials: initials
+      score: score,
+      initials: initials,
     };
 
     // save to localstorage
+    highscores.push(newScore)
+    localStorage.setItem("highscores", JSON.stringify(highscores))
 
     // redirect to next page
     window.location.href = "highscores.html";
